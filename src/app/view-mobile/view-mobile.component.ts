64f443dfb28cf07a44e12094057230e85f96e1fb
Ashutosh Pradhan <ashutosh.pradhan@globallogic.com>
2024-06-04 11:47:29 +0530

    View Mobile Done

--- src/app/view-mobile/view-mobile.component.ts
+++ src/app/view-mobile/view-mobile.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+
+interface Mobile {
+  brand: string;
+  price: number;
+  color: string;
+  storage: number;
+  description: string;
+  model: string;
+}
 
 @Component({
   selector: 'app-view-mobile',
   templateUrl: './view-mobile.component.html',
-  styleUrl: './view-mobile.component.css'
+  styleUrls: ['./view-mobile.component.css']
 })
-export class ViewMobileComponent {
+export class ViewMobileComponent implements OnInit {
+  mobiles: Mobile[] = [];
+  errorMessage: string = '';
 
+  constructor(private appService: AppService) { }
+
+  ngOnInit(): void {
+    this.appService.viewMobile().subscribe(
+      (data: Mobile[]) => {
+        this.mobiles = data;
+      },
+      (error) => {
+        this.errorMessage = 'Failed to load mobile data. Please try again later.';
+      }
+    );
+  }
 }
